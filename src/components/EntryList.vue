b30760a77acaa5089faa6d5f8b7a0bcfe80165d7
tal0311 <tal.amit0311@gmail.com>
2024-09-20 01:22:56 +0300

    handle multi entries for day

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -3,18 +3,19 @@
         <details>
             <summary class="grid grid-dir-col">
                 <h4>{{ day }}/9/2024 </h4>
-                <span class="icon-svg" v-if="getEntryForDay(day)" v-html="$svg('indication')"></span>
+                <span class="icon-svg" v-if="getEntriesForDay(day).length" v-html="$svg('indication')"></span>
             </summary>
-            <p v-if="getEntryForDay(day)">Details about the day</p>
-            <div v-if="getEntryForDay(day)">
-
-                <p>Date: {{ convertToReadableDate(getEntryForDay(day).date) }}</p>
-                <p>Start Time: {{ convertToReadableTime(getEntryForDay(day).startTime) }}</p>
-                <p>End Time: {{ convertToReadableTime(getEntryForDay(day).endTime) }}</p>
-                <p>Project ID: {{ getEntryForDay(day).projectId }}</p>
-                <p>Notes: {{ getEntryForDay(day).notes }}</p>
-
-            </div>
+            <p v-if="getEntriesForDay(day).length">Details about the day</p>
+            <ul v-if="getEntriesForDay(day).length">
+                <li v-for="entry in getEntriesForDay(day)" :key="entry._id">
+                    <p>Date: {{ convertToReadableDate(entry.date) }}</p>
+                    <p>Start Time: {{ convertToReadableTime(entry.startTime) }}</p>
+                    <p>End Time: {{ convertToReadableTime(entry.endTime) }}</p>
+                    <p>Project ID: {{ entry.projectId }}</p>
+                    <p>Notes: {{ entry.notes }}</p>
+                    <hr />
+                </li>
+            </ul>
             <p v-else>No entry for this day</p>
         </details>
     </section>
@@ -29,8 +30,8 @@ const props = defineProps(['entries']);
 
 const days = ref(daysInMonth(9, 2024));
 
-function getEntryForDay(day) {
-    return props.entries.find(entry => {
+function getEntriesForDay(day) {
+    return props.entries.filter(entry => {
         const entryDate = new Date(entry.date).getDate();
         return entryDate === day;
     });
@@ -38,11 +39,14 @@ function getEntryForDay(day) {
 </script>
 
 <style scoped>
-details{
-    
+details {
     padding: 1rem;
     border: 1px solid #ccc;
     border-radius: 0.5rem;
     margin-block-end: 0.5rem;
 }
+hr {
+    margin: 1rem 0;
+}
 </style>
+
